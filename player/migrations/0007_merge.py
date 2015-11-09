94d6e4fee7d5186449f38e9afc5d27c59b388e0b
ClearSpirit <Jingao@Spirit.local>
2015-11-09 20:12:23 +0800

    final

---
+++ player/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('player', '0005_auto_20150806_0757'),
+        ('player', '0006_auto_20150806_1633'),
+    ]
+
+    operations = [
+    ]
